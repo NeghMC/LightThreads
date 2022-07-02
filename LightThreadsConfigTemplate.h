24d18f884979766ed32b9feb1ef56b756fb0870d
gswistak <neghmc@live.com>
2022-07-02 13:17:28 +0200

    fixed config template

---
+++ LightThreadsConfigTemplate.h
@@ -0,0 +1,8 @@
+/*
+ * Remove "Template" from the file name and uncomment required modules
+ */
+
+//#define LT_USE_DELAY
+//#define LT_USE_BINATY_SEMAPHORES
+//#define LT_USE_IDLE_HANDLER
+//#define LT_USE_NOTIFICATIONS
